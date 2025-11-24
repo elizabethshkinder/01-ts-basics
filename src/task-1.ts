4b76302b2c47a69e3da91576807a62bed02d12dc
Elizabeth Shkinder <elizabethshkinder@unknown.home>
2025-11-24 13:18:10 +0000

    initial settings

---
+++ src/task-1.ts
@@ -0,0 +1,13 @@
+// Завдання:
+// Додай до кожної змінної явну типізацію.
+// Переконайся, що TypeScript правильно визначає тип, і не видає помилку.
+// Залиш назви змінних і їх значення без змін.
+
+const name = "Alice";
+const age = 30;
+const isOnline = true;
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Online: ${isOnline}`);
+
